b2782dc28a8e3d96c02c6da575268b2b09a9d625
tidingdbruh <tidingdbruh@gmail.com>
2026-08-27 23:09:38 +0530

    Adding

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -168,6 +168,23 @@ def call(Map configMap) {
                     }
                 }
             }
+            stage('Docker Push') {
+                steps {
+                    script {
+                        sh """
+                            aws ecr get-login-password --region us-east-1 | \
+                            docker login --username AWS --password-stdin \
+                            ${acc_id}.dkr.ecr.us-east-1.amazonaws.com
+
+                            docker tag ${component}:${appVersion} \
+                            ${acc_id}.dkr.ecr.us-east-1.amazonaws.com/roboshop/${component}:${appVersion}
+
+                            docker push \
+                            ${acc_id}.dkr.ecr.us-east-1.amazonaws.com/roboshop/${component}:${appVersion}
+                        """
+                    }
+                }
+            }
         }
     }
 }
